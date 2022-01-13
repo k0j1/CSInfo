7d12a4ad49e20c4c83d8451a77a1882bc54ecf0a
k0j1 <k0j123t@gmail.com>
2022-01-13 23:33:57 +0900

    塾情報配信アプリVer.1.0.3 ・サイト名：柏の塾情報ナビに変更 ・プライバシーポリシーのURL変更 ・アイコン変更

--- app/src/main/java/com/takaharabooks/app/csinfo/ui/about/AboutPageFragment.java
+++ app/src/main/java/com/takaharabooks/app/csinfo/ui/about/AboutPageFragment.java
@@ -71,7 +71,7 @@ public class AboutPageFragment extends Fragment {
         elem.setIconDrawable(R.drawable.ic_icon_article);
         elem.setAutoApplyIconTint(true);
         //elem.setIconTint(R.color.about_twitter_color);
-        Uri uri = Uri.parse("https://www.yamatocreation.info/coinnews");
+        Uri uri = Uri.parse("https://www.yamatocreation.info/%E6%9F%8F%E3%81%AE%E5%A1%BE%E3%83%8A%E3%83%93%E3%83%97%E3%83%A9%E3%82%A4%E3%83%90%E3%82%B7%E3%83%BC%E3%83%9D%E3%83%AA%E3%82%B7%E3%83%BC");
         Intent goHP = new Intent(Intent.ACTION_VIEW, uri);
         elem.setIntent(goHP);
         return elem;
